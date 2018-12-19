8db68280285bea2b6479fa5b962024349cff551a
Anton Paronko <iparonko@gmail.com>
2018-12-20 01:43:27 +0300

    Cookie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,10 +10,10 @@ public class Main {
         headersNameGitLabLogin.add("Cookie");
 
         ArrayList<String> headersValueGitLabLogin = new ArrayList<String>();
-        headersValueGitLabLogin.add("JSESSIONID.1fb7cb68=node0dol0riptjnlu9h0o6zp8rxra14457.node0"); //Cookie
+        headersValueGitLabLogin.add("кука"); //Cookie
 
         HttpURLConnection responseOpenPage = Api.execute(
-                "http://build.youdo.sg/job/staging/job/youdo_android_testing/579/artifact/report/report.html",
+                "ссылка на отчет",
                 "GET",
                 headersNameGitLabLogin,
                 headersValueGitLabLogin
