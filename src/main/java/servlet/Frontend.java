df8c21a2ded7f3e037d4829e563e0fb5600b92f0
anton <iparonko@gmail.com>
2019-12-17 01:07:35 +0300

    fix

--- src/main/java/servlet/Frontend.java
+++ src/main/java/servlet/Frontend.java
@@ -4,15 +4,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
 import static executor.ExecutorMethods.checkAndSaveNewReport;
 
 public class Frontend extends HttpServlet {
     private String login = "";
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String urlRequest = request.getRequestURI();
         if(urlRequest.equals("/login")) {
-            System.out.println("login");
+            response.sendError(300,"login");
         } else if(urlRequest.equals("/checkandsavenewreport")) {
             checkAndSaveNewReport();
         }
