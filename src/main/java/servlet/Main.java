559cd341215a058bd9fb22cdaf717fd56dc73f84
Paronko <iparonko@gmail.com>
2019-12-18 17:50:47 +0300

    add log db

--- src/main/java/servlet/Main.java
+++ src/main/java/servlet/Main.java
@@ -25,7 +25,7 @@ public class Main {
 
         Frontend frontend = new Frontend();
 
-        Server server = new Server(new InetSocketAddress("ovz9.iparonko.me78p.vps.myjino.ru", 5777));
+        Server server = new Server(new InetSocketAddress("paronko.ru", 5777));
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         server.setHandler(context);
         context.addServlet(new ServletHolder(frontend), "/checkandsavenewreport");
