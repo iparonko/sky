f11f33f05cb8aacf79ac55b2de6d0976ae50d2c6
Paronko <iparonko@gmail.com>
2019-12-18 17:30:56 +0300

    add log db

--- src/main/java/log/LoggerInfo.java
+++ src/main/java/log/LoggerInfo.java
@@ -1,11 +1,33 @@
 package log;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import settings.Accounts;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 public class LoggerInfo {
-    private static Logger log = Logger.getLogger(Logger.class.getName());
+    private static Logger log = getLogger();
+
+    private static Logger getLogger() {
+        Logger logger = Logger.getLogger(Logger.class.getName());
+        Properties prop = new Properties();
+        try {
+            prop.load(new FileReader("/root/sky/target/classes/log4j.properties"));
+            //prop.load(new FileReader("/home/aparonko/IdeaProjects/sky/src/main/resources/log4j.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        prop.put("log4j.appender.db.user", "iparonko_notes");
+        prop.put("log4j.appender.db.password", Accounts.PASSWORD_SQL);
+        PropertyConfigurator.configure(prop);
+        return logger;
+    }
 
     public static void logException(String message, Exception e) {
+        message = message.replaceAll("'", "%27");
         log.fatal("[Исключение] " + message, e);
     }
 
@@ -14,10 +36,12 @@ public class LoggerInfo {
     }
 
     public static void log(String message) {
+        message = message.replaceAll("'", "%27");
         log.info(message);
     }
 
     public static void logError(String message) {
+        message = message.replaceAll("'", "%27");
         log.error("[Ошибка] " + message);
     }
 }
